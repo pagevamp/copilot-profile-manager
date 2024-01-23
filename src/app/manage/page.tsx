f1e7dc1a60e3bfccb60d6503aafe0a70aa767486
aatbip <anantabipal2010@gmail.com>
2024-01-23 20:11:54 +0545

    add: empty state fallback page

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,5 +1,7 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import { ManagePageContainer } from './views/ManagePageContainer';
+import { SimpleButton } from '@/components/styled/SimpleButton';
+// import { EmptyStateFallback } from './views/EmptyStateFallback';
 
 export default function Home() {
   return (
@@ -11,6 +13,18 @@ export default function Home() {
       <Typography variant="xl">Manage your profile</Typography>
 
       <ManagePageContainer />
+
+      <Stack direction="column" mt={16} rowGap={4}>
+        <Typography variant="xl">Other settings</Typography>
+        <Stack direction="row" columnGap={4}>
+          <SimpleButton>
+            <Typography variant="md">Set a payment method</Typography>
+          </SimpleButton>
+          <SimpleButton>
+            <Typography variant="md">Go to account settings</Typography>
+          </SimpleButton>
+        </Stack>
+      </Stack>
     </Box>
   );
 }
---
+++ src/components/styled/SimpleButton.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { styled, Box } from '@mui/material';
+
+export const SimpleButton = styled(Box)(({ theme }) => {
+  return {
+    padding: '4px 8px',
+    borderRadius: theme.shape.radius100,
+    border: `1px solid ${theme.color.borders.border}`,
+    background: theme.color.base.white,
+    cursor: 'pointer',
+  };
+});
