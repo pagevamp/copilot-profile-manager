0a34ed6c47dfe68a770ea2750034f5dc5294e804
Sajjan Shrestha <sajjan@outside.tech>
2024-01-23 13:16:42 +0545

    Api setup & Custom Field Access API (#7)
    Api setup & Custom Field Access API

---
+++ src/types/common.ts
@@ -0,0 +1,55 @@
+import { z } from 'zod';
+
+export const MeResponseSchema = z.object({
+  id: z.string(),
+  givenName: z.string(),
+  familyName: z.string(),
+  email: z.string(),
+  portalName: z.string(),
+});
+export type MeResponse = z.infer<typeof MeResponseSchema>;
+
+export const ClientResponseSchema = z.object({
+  id: z.string(),
+  givenName: z.string(),
+  familyName: z.string(),
+  email: z.string(),
+  companyId: z.string(),
+  customFields: z.record(z.string(), z.union([z.string(), z.array(z.string())])).nullable(),
+});
+export type ClientResponse = z.infer<typeof ClientResponseSchema>;
+
+export const ClientsResponseSchema = z.object({
+  data: z.array(ClientResponseSchema).nullable(),
+});
+export type ClientsResponse = z.infer<typeof ClientsResponseSchema>;
+
+export const CompanyResponseSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  iconImageUrl: z.string(),
+});
+export type CompanyResponse = z.infer<typeof CompanyResponseSchema>;
+
+export const CustomFieldSchema = z.object({
+  id: z.string(),
+  key: z.string(),
+  name: z.string(),
+  type: z.string(),
+  order: z.number(),
+  object: z.string(),
+  options: z
+    .array(
+      z.object({
+        id: z.string(),
+        key: z.string(),
+        label: z.string(),
+        color: z.string(),
+      }),
+    )
+    .optional(),
+});
+export const CustomFieldResponseSchema = z.object({
+  data: z.array(CustomFieldSchema).nullable(),
+});
+export type CustomFieldResponse = z.infer<typeof CustomFieldResponseSchema>;
