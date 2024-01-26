3af1adc0de6cb6208b7af46c75e2a99b077ad563
Ananta Bipal <anantabipal2010@gmail.com>
2024-01-26 10:30:14 +0545

    Out 26 Setup Context API (#5)
    * add: context api setup
    
    * fix minor bug
    
    * Out 27 Setup toggle functionality and responsiveness (#6)
    
    * add: toggle and responsiveness
    
    * fix minor bug
    
    * fix minor bug
    
    * fix minor bug
    
    * fix: header layout text heading wrapping
    
    * add: base setup for table with brand styles
    
    * add: base setup for table with brand styles
    
    * add: extended table component
    
    * add: extended table component
    
    * fix: table width fits the screen
    
    * add: comparator function for complex object sorting
    
    * add: history cell renderer components
    
    * add: history cell renderer components
    
    * add: search and sort capabalities on the table
    
    * add: search and sort capabalities on the table
    
    * add: autocomplete
    
    * add: empty state fallback page
    
    * add: empty state fallback page
    
    * Changed Next.js version to latest
    
    Next.js version has been changed to 'latest'. Using the prior
    version was throwing a bug in the production. To replicate the
    bug, simply downgrade to the previous version and try building the
    app.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import ThemeRegistry from './ThemeRegistry';
+import { AppContextProvider } from '@/context';
+import { ToggleDecider } from '@/hoc/ToggleDecider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,9 +15,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <ThemeRegistry options={{ key: 'mui' }}>
-        <body className={inter.className}>{children}</body>
-      </ThemeRegistry>
+      <AppContextProvider>
+        <ThemeRegistry options={{ key: 'mui' }}>
+          <body className={inter.className}>
+            <ToggleDecider>{children}</ToggleDecider>
+          </body>
+        </ThemeRegistry>
+      </AppContextProvider>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { Box, Stack, Typography } from '@mui/material';
 import { Sidebar } from './views/Sidebar';
 import MainSection from './views/MainSection';
+import { SidebarDecider } from '@/hoc/SidebarDecider';
 
 export default function Home() {
   return (
     <Stack direction="row">
       <MainSection />
 
-      <Sidebar />
+      <SidebarDecider>
+        <Sidebar />
+      </SidebarDecider>
     </Stack>
   );
 }
