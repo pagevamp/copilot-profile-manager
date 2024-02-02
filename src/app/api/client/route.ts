77c70f6e46a754b16245be02503cf8fa9bf2820c
aatbip <anantabipal2010@gmail.com>
2024-02-02 11:00:47 +0545

    added client api

---
+++ src/app/api/client/route.ts
@@ -0,0 +1,25 @@
+import { respondError } from '@/utils/common';
+import { CopilotAPI } from '@/utils/copilotApiUtils';
+import { NextResponse, NextRequest } from 'next/server';
+import { z } from 'zod';
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const clientId = searchParams.get('clientId');
+  const token = searchParams.get('token');
+  if (!token) {
+    return respondError('Missing token', 422);
+  }
+  if (!clientId) {
+    respondError('Missing client Id', 422);
+  }
+  const copilotClient = new CopilotAPI(z.string().parse(token));
+  try {
+    const client = await copilotClient.getClient(z.string().parse(clientId));
+
+    return NextResponse.json({ data: client });
+  } catch (error) {
+    console.log(error);
+    return respondError('Client not found.', 404);
+  }
+}
