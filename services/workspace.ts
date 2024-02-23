74dfa1694da26884ba4d14f556377cb3596c7ece
Rojan Rajbhandari <rrojan20@gmail.com>
2024-02-23 13:29:40 +0545

    fix: fetch settings directly

--- services/workspace.ts
+++ services/workspace.ts
@@ -1,16 +1,11 @@
 import { apiUrl } from '@/config';
 import { APIProps } from '@/types/api';
 import { WorkspaceResponse } from '@/types/common';
+import { CopilotAPI } from '@/utils/copilotApiUtils';
+import { z } from 'zod';
 
 // Fetch workspace from API
-export async function getWorkspaceInfo({ token, portalId }: APIProps): Promise<WorkspaceResponse> {
-  const res = await fetch(`${apiUrl}/api/workspace?token=${token}`, {
-    next: { tags: ['workspace'] },
-  });
-
-  if (!res.ok) {
-    throw new Error('Something went wrong in getWorkspaceInfo');
-  }
-
-  return (await res.json()) as WorkspaceResponse;
+export async function getWorkspaceInfo({ token }: APIProps): Promise<WorkspaceResponse> {
+  const copilotClient = new CopilotAPI(z.string().parse(token));
+  return await copilotClient.getWorkspace();
 }
