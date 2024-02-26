8a86c1feee150d736c286ad3a20879b097d62ee5
Rojan Rajbhandari <rrojan20@gmail.com>
2024-02-26 19:39:09 +0545

    refactor: clean up code

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -7,35 +7,16 @@ import { OverridableStringUnion } from '@mui/types';
 import { Variant } from '@mui/material/styles/createTypography';
 import Link from 'next/link';
 
-type ButtonModes = 'button' | 'link';
-
 interface ButtonProps {
   children: string | JSX.Element;
-  typographyVariant?: OverridableStringUnion<Variant | 'inherit', TypographyPropsVariantOverrides>;
-  mode?: ButtonModes;
-  href?: string;
   route?: string;
+  typographyVariant?: OverridableStringUnion<Variant | 'inherit', TypographyPropsVariantOverrides>;
+  onClick?: () => void;
 }
 
-const Button = ({ mode = 'button', children, href, route, typographyVariant = 'md' }: ButtonProps) => {
-  let onClick;
-  if (mode === 'button' && route) {
-    onClick = () => {
-      window.parent.postMessage({ type: 'history.push', route }, '*');
-    };
-  }
-  if (mode === 'link') {
-    // Use mode link if you want Next to route to another url but also prefetch that page
-    return (
-      <Link href={href || '#'} style={{ textDecoration: 'none' }}>
-        <SimpleButton>
-          <Typography variant={typographyVariant}>{children}</Typography>
-        </SimpleButton>
-      </Link>
-    );
-  }
+const Button = ({ children, route, onClick, typographyVariant = 'md' }: ButtonProps) => {
+  onClick = onClick || (() => window.parent.postMessage({ type: 'history.push', route: route || '#' }, '*'));
 
-  // Use mode button if you have custom behaviour / don't want prefetching to work
   return (
     <SimpleButton onClick={onClick}>
       <Typography variant={typographyVariant}>{children}</Typography>
