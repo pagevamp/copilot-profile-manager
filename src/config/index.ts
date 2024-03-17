4f2bef9e5430be3b551796dc1de382112f97ec34
Rojan Rajbhandari <rrojan20@gmail.com>
2024-03-17 12:41:21 +0545

    feat: integrate sentry for error monitoring

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,6 @@
 export const copilotAPIUrl = process.env.COPILOT_API_URL || '';
 export const copilotAPIKey = process.env.COPILOT_API_KEY || '';
 export const apiUrl = `${process.env.VERCEL_ENV === 'development' ? 'http://' : 'https://'}${process.env.VERCEL_URL}`;
+export const SentryConfig = {
+  DSN: process.env.NEXT_PUBLIC_SENTRY_DSN || '',
+};
